29c52b6594fcb33f731b7556d568cd4db345944c
Callum <callum@clow.nz>
2024-08-11 00:20:21 +1200

    had some git issues, should be fixed now

---
+++ Personal Portfolio/src/views/ProjectsView.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="projects-view">
+		<h1>Projects</h1>
+		<ul>
+			<li v-for="project in projects" :key="project.id">
+				{{ project.name }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
