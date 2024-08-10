157592b55703f9314aa049abfdb1c4bb71843da0
Callum <callum@clow.nz>
2024-08-10 23:38:40 +1200

    button implemented, branch done

--- Personal Portfolio/src/views/HomeView.vue
+++ Personal Portfolio/src/views/HomeView.vue
@@ -6,6 +6,9 @@
 		<h1> Hi, I'm <span class="highlight">Callum</span>! </h1>
 		<h1> Welcome to my portfolio. </h1>
 	</div>
+	<div class="continue-button">
+		<RouterLink to="/about" class="continue-text">Continue</RouterLink>
+	</div>
 </template>
 
 <style scoped>
@@ -21,5 +24,23 @@ h1 {
 
 .highlight {
 	color: var(--secondary-color);
+}
+
+.continue-button {
+	background-color: var(--border-color);
+	color: var(--text-color);
+	padding: 1rem 2rem;
+	border-radius: 5px;
+	font-size: 1.5rem;
+	cursor: pointer;
+	max-width: 200px;
+	margin: 2rem;
+}
+.continue-button:hover {
+	background-color: var(--secondary-color);
+}
+.continue-text {
+	text-decoration: none;
+	color: var(--text-color);
 }
 </style>
