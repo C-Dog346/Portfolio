86692ca6dc9ad3c47c8e12d746e3b8566b9d97c4
Callum <callum@clow.nz>
2024-08-10 22:50:38 +1200

    god damn it I did the about - I need to remember I'm tryna do branches lol

--- Personal Portfolio/src/views/AboutView.vue
+++ Personal Portfolio/src/views/AboutView.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="so-far">
+      <h1> About me so far </h1>
+      <p class="blurb">
+        I am a full-stack web developer with a passion for creating beautiful and functional websites. I have experience
+        with HTML, CSS, JavaScript, and Vue.js. I am always looking to learn new technologies and improve my skills.
+      </p>
+    </div>
+    <div class="image">
+      <img src="../assets/about_pic.jpg" alt="Selfie" />
+    </div>
+    <div class="future">
+      <h1> My future goals </h1>
+      <p class="blurb">
+        I am currently working on improving my skills with Vue.js and learning more about back-end development. I am
+        excited to continue learning and growing as a developer.
+      </p>
+    </div>
   </div>
 </template>
 
 <style>
+.about {
+  margin: 2rem;
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+}
+
+.blurb {
+  font-size: 1.5rem;
+  color: var(--text-color);
+  max-width: 600px;
+}
 </style>
