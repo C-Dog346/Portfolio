6bd2f1909c6436d2f1e53f3ba9037ad8a1cdd0cf
Callum <callum@clow.nz>
2024-08-11 23:31:13 +1200

    ProjectExample prototype made. Test successful

---
+++ Personal Portfolio/src/components/ProjectExample.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+		name: {
+			type: String,
+			required: true
+		},
+		image: {
+			type: String,
+			required: true
+		},
+		description: {
+			type: String,
+			required: true
+		}
+	
+});
+
+</script>
+
+<template>
+	<div class="project">
+		<img :src="props.image" alt="Project Image" />
+		<p>{{ props.description }}</p>
+	</div>
+</template>
+
+<style scoped>
+.project {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+</style>
--- Personal Portfolio/src/views/ProjectsView.vue
+++ Personal Portfolio/src/views/ProjectsView.vue
@@ -1,18 +1,45 @@
+<script setup>
+import ProjectExample from '@/components/ProjectExample.vue';
+
+const projects = [
+	{
+		id: 1,
+		name: 'Project 1',
+		image: 'https://via.placeholder.com/150',
+		description: 'This is project 1'
+	},
+	{
+		id: 2,
+		name: 'Project 2',
+		image: 'https://via.placeholder.com/150',
+		description: 'This is project 2'
+	},
+	{
+		id: 3,
+		name: 'Project 3',
+		image: 'https://via.placeholder.com/150',
+		description: 'This is project 3'
+	}
+];
+
+</script>
+
 <template>
 	<div class="projects-view">
 		<h1>Projects</h1>
 		<ul>
 			<li v-for="project in projects" :key="project.id">
-				{{ project.name }}
+				<ProjectExample 
+					:name="project.name" 
+					:image="project.image" 
+					:description="project.description"></ProjectExample>
 			</li>
 		</ul>
 	</div>
 </template>
 
-<script>
-
-</script>
-
 <style scoped>
-
+ul {
+	list-style: none;
+}
 </style>
