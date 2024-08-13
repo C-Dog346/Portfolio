4ef47f5e9f3d262ebd302377e05db16d0f270466
Callum <callum@clow.nz>
2024-08-13 22:50:17 +1200

    projects done for now. Added github link and an actual image for the first project

--- Personal Portfolio/src/components/ProjectExample.vue
+++ Personal Portfolio/src/components/ProjectExample.vue
@@ -1,26 +1,38 @@
 <script setup>
 const props = defineProps({
-		name: {
-			type: String,
-			required: true
-		},
-		image: {
-			type: String,
-			required: true
-		},
-		description: {
-			type: String,
-			required: true
-		}
-	
+	name: {
+		type: String,
+		required: true
+	},
+	image: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	github: {
+		type: String,
+		required: true
+	}
+
 });
 
 </script>
 
 <template>
 	<div class="project">
-		<img :src="props.image" alt="Project Image" />
-		<p>{{ props.description }}</p>
+		<h1> {{ props.name }} </h1>
+		<div class="image">
+			<img :src="props.image" alt="Project Image" />
+		</div>
+		<p class="description">{{ props.description }}</p>
+		<div class="links">
+			<a :href="props.github" target="_blank"><img class="icon" src="../assets/GitHubLogo.png"
+					alt="GitHub project link" /></a>
+		</div>
+
 	</div>
 </template>
 
@@ -28,6 +40,31 @@ const props = defineProps({
 .project {
 	display: flex;
 	flex-direction: column;
-	align-items: center;
+	border: 1px solid --border-color;
+}
+
+.image {
+	border: 1px solid var(--border-color);
+}
+
+p.description {
+	margin-top: 1rem;
+	display: flex;
+	justify-content: center;
+	margin-bottom: 4rem;
+}
+
+.icon {
+	width: 50px;
+	height: 50px;
+}
+
+.links {
+	display: flex;
+	justify-content: center;
+}
+
+a {
+	max-width: fit-content;
 }
 </style>
--- Personal Portfolio/src/views/ProjectsView.vue
+++ Personal Portfolio/src/views/ProjectsView.vue
@@ -4,21 +4,24 @@ import ProjectExample from '@/components/ProjectExample.vue';
 const projects = [
 	{
 		id: 1,
-		name: 'Project 1',
-		image: 'https://via.placeholder.com/150',
-		description: 'This is project 1'
+		name: 'Islands Challenge',
+		image: 'src/assets/IslandsProject.png',
+		description: 'This is a project that I made for the islands challenge.',
+		github: 'https://github.com/C-Dog346/Islands-Challenge'
 	},
 	{
 		id: 2,
 		name: 'Project 2',
 		image: 'https://via.placeholder.com/150',
-		description: 'This is project 2'
+		description: 'This is project 2',
+		github: 'https://github.com/C-Dog346/Islands-Challenge'
 	},
 	{
 		id: 3,
 		name: 'Project 3',
 		image: 'https://via.placeholder.com/150',
-		description: 'This is project 3'
+		description: 'This is project 3',
+		github: 'https://github.com/C-Dog346/Islands-Challenge'
 	}
 ];
 
@@ -29,10 +32,8 @@ const projects = [
 		<h1>Projects</h1>
 		<ul>
 			<li v-for="project in projects" :key="project.id">
-				<ProjectExample 
-					:name="project.name" 
-					:image="project.image" 
-					:description="project.description"></ProjectExample>
+				<ProjectExample :name="project.name" :image="project.image" :description="project.description"
+					:github="project.github"></ProjectExample>
 			</li>
 		</ul>
 	</div>
