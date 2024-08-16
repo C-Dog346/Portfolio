7400b4901b27b47abddf010f42af0cafaa37f979
Callum <callum@clow.nz>
2024-08-16 17:50:00 +1200

    Contact form functionality complete

---
+++ Personal Portfolio/src/components/ContactForm.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref } from 'vue';
+import emailjs from '@emailjs/browser';
+
+const form = ref(null);
+
+const sendEmail = () => {
+    emailjs
+        .sendForm('service_5cba85g', 'template_b36jwni', form.value, {
+            publicKey: 'f-k6FbmkYdrA0tDFr',
+        })
+        .then(
+            () => {
+                console.log('SUCCESS!');
+            },
+            (error) => {
+                console.log('FAILED...', error.text);
+            }
+        );
+};
+
+</script>
+
+<template>
+    <form ref="form" @submit.prevent="sendEmail">
+        <input type="text" name="user_name" placeholder="Name *" required>
+        <input type="email" name="user_email" placeholder="Email *" required>
+        <input type="organisation" id="user_organisation" placeholder="Organisation">
+        <textarea name="message" placeholder="Message *" required></textarea>
+        <button type="submit" value="Send">Submit</button>
+    </form>
+</template>
+
+<style>
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+input,
+textarea {
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 0px;
+    border-radius: 4px;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    min-width: 30%;
+    max-width: 60%;
+}
+
+textarea {
+    min-height: 150px;
+}
+
+button {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    max-width: 400px;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
+
+button:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+}
+</style>
--- Personal Portfolio/src/views/ContactView.vue
+++ Personal Portfolio/src/views/ContactView.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { ref } from 'vue';
+import ContactForm from '@/components/ContactForm.vue';
 
 const name = ref('');
 const email = ref('');
 const organisation = ref('');
 const message = ref('');
+
+
 
 // Submit function
 const submitForm = () => {
@@ -17,79 +20,17 @@ const submitForm = () => {
 	};
 	// Log the form data
 	console.log(formData);
-
-	// send email to target email address
-	
 };
 </script>
 
 <template>
-	<div>
 		<h1>Contact</h1>
-		<form @submit.prevent="submitForm">
-			<div>
-				<div>
-					<input type="text" id="name" v-model="name" placeholder="Name *" required>
-				</div>
-				<div>
-					<input type="email" id="email" v-model="email" placeholder="Email *" required>
-				</div>
-				<div>
-					<textarea id="message" v-model="Organisation" placeholder="Organisation"></textarea>
-				</div>
-				<div>
-					<textarea id="message" v-model="message" placeholder="Message *" required></textarea>
-				</div>
-				<button type="submit">Submit</button>
-			</div>
-		</form>
-	</div>
+	<ContactForm></ContactForm>
 </template>
 
 <style scoped>
 h1 {
 	font-size: 4rem;
 	margin-bottom: 20px;
-}
-
-form {
-	display: flex;
-	flex-direction: column;
-	margin: 0 auto;
-}
-
-label {
-	font-weight: bold;
-	margin-bottom: 5px;
-}
-
-input,
-textarea {
-	padding: 10px;
-	margin-bottom: 10px;
-	border: 1px solid #ccc;
-	border-radius: 4px;
-	font-family: 'Roboto', sans-serif;
-	font-size: 14px;
-	min-width: 40%;
-}
-
-button {
-	padding: 10px 20px;
-	background-color: #007bff;
-	color: #fff;
-	border: none;
-	border-radius: 4px;
-	cursor: pointer;
-	max-width: 400px;
-}
-
-button:hover {
-	background-color: #0056b3;
-}
-
-button:disabled {
-	background-color: #ccc;
-	cursor: not-allowed;
 }
 </style>
