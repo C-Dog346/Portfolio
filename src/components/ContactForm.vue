b70fa4b0250138fcbbd83148befa9d62608abd8b
Callum <callum@clow.nz>
2024-09-10 00:01:04 +1200

    hover effect almost there

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,6 +5,7 @@ import emailjs from '@emailjs/browser';
 const form = ref(null);
 
 const sendEmail = () => {
+    let messageDiv = form.value.querySelector('.status-message')
     emailjs
         .sendForm('service_5cba85g', 'contact_form', form.value, {
             publicKey: 'f-k6FbmkYdrA0tDFr',
@@ -13,10 +14,13 @@ const sendEmail = () => {
             () => {
                 console.log('SUCCESS!');
                 form.value.reset();
-                form.value.querySelector('.status-message').innerHTML = 'Your message has been sent!';
+                messageDiv.classList.add('success');
+                messageDiv.innerHTML = 'Your message has been sent!';
             },
             (error) => {
                 console.log('FAILED...', error.text);
+                messageDiv.classList.add('error');
+                messageDiv.innerHTML = 'There was an error sending your message. Please try again later.';
             }
         );
 };
@@ -29,7 +33,9 @@ const sendEmail = () => {
         <input type="email" name="user_email" placeholder="Email *" required>
         <input type="organisation" id="user_organisation" placeholder="Organisation">
         <textarea name="message" placeholder="Message *" required></textarea>
-        <button type="submit" value="Send">Submit</button>
+        <div class="submit-button">
+            <button type="submit" value="Send">Submit</button>
+        </div>
         <div class="status-message"></div>
     </form>
 </template>
@@ -58,27 +64,63 @@ textarea {
     min-height: 150px;
 }
 
+div.submit-button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+button::after {
+    display: block;
+    content: '';
+    height: 2px;
+    bottom: 1px;
+    top: 0%;
+    background-color: var(--secondary-color);
+    width: 100%;
+    transition: all 1.5;
+    transform-origin: bottom;
+    z-index: -1;
+}
+
 button {
     padding: 10px 20px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 4px;
+    padding-bottom: 0px;
+    font-size: 16px;
+    color: white;
+    background-color: transparent;
+    position: relative;
+    overflow: hidden;
     cursor: pointer;
-    max-width: 400px;
+    z-index: 1;
+    border: transparent;
 }
 
-button:hover {
-    background-color: #0056b3;
+button::before {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--secondary-color);
+    transition: top 0.5s ease;
+    z-index: -1;
 }
 
-button:disabled {
-    background-color: #ccc;
-    cursor: not-allowed;
+button:hover::before {
+    top: 0;
 }
 
 .status-message {
     margin-top: 10px;
+}
+
+.status-message.success {
     color: #00DD50;
+}
+
+.status-message.error {
+    color: #FF0000;
 }
 </style>
