353482420d4dc5cb92f93de6447dd1d4a1135a83
Callum <callum@clow.nz>
2024-09-19 12:32:30 +1200

    remove debugging commits and improved 'My Technologies' title

--- src/components/ImageBanner.vue
+++ src/components/ImageBanner.vue
@@ -19,16 +19,15 @@ const speed = 90;
 
 const startAutoScroll = () => {
   intervalId = setInterval(() => {
-    translateX.value -= speed; // Move left
-    // When translateX reaches the point where the second set starts, reset to the original images
+    translateX.value -= speed;
     if (Math.abs(translateX.value) >= (props.images.length + 1) * speed) {
-      isTransitioning.value = false; // Disable transition for seamless reset
-      translateX.value = 0; // Reset to the start of the original images
+      isTransitioning.value = false;
+      translateX.value = 0
       setTimeout(() => {
-        isTransitioning.value = true; // Re-enable transition
+        isTransitioning.value = true;
       }, 50);
     }
-  }, 3000); // Adjust interval for the desired scroll speed
+  }, 3000)
 };
 
 const scrollStyle = computed(() => {
@@ -56,11 +55,9 @@ onBeforeUnmount(() => {
   <div class="image-banner">
     <div class="image-banner-container">
       <div class="image-banner-content">
-        <!-- First set of images -->
         <div v-for="(image, index) in props.images" :key="'set1-' + index" class="image-banner-item" :style="scrollStyle">
           <img :src="image.image" alt="Banner Image" />
         </div>
-        <!-- Second set of images for seamless loop -->
         <div v-for="(image, index) in props.images" :key="'set2-' + index" class="image-banner-item" :style="scrollStyle">
           <img :src="image.image" alt="Banner Image" />
         </div>
@@ -70,8 +67,13 @@ onBeforeUnmount(() => {
 </template>
 
 <style scoped>
+.image-banner-title {
+  font-size: 2rem;
+  color: var(--text-color);
+  margin: 2rem 0;
+}
 .image-banner {
-  overflow: hidden; /* Ensure only one set of images is visible at a time */
+  overflow: hidden;
 }
 .image-banner-container {
   display: flex;
@@ -80,9 +82,9 @@ onBeforeUnmount(() => {
 }
 .image-banner-content {
   display: flex;
-  width: max-content; /* Ensures the container grows to fit all items */
+  width: max-content;
 }
 .image-banner-item img {
-  width: 90px; /* Each image takes the full width of the banner */
+  width: 90px;
 }
 </style>
